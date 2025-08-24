5dd436ec18783be4cbab740362bea6026b519596
vitaliy <vitalijurah@gmail.com>
2025-08-24 16:13:34 +0300

    upd

--- front/source/js/components/select.js
+++ front/source/js/components/select.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const selects = document.querySelectorAll('.custom-select');
 
     selects.forEach(function (select) {
+        const placeholder = select.querySelector('.placeholder')?.textContent
+
         const customSelect = new CustomSelect(select, {
             mode: 'single',
-
+            placeholder,
             name: select.getAttribute('data-name') || '',
             hideOnSelect: true,
             hideOnClear: true,
