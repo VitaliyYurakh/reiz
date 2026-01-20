ec6419be59a637661cd38666b7d2b0115df58bb3
MiG0oo <maryanyedynak@gmail.com>
2026-01-20 09:22:11 +0200

    upd

--- front/src/app/robots.ts
+++ front/src/app/robots.ts
@@ -3,19 +3,25 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   const base = process.env.NEXT_PUBLIC_SITE_URL ?? "https://reiz.com.ua";
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        // Deleted/non-existent car pages
-        "/cars/1-bmw-x3-2023",
-        "/ru/cars/1-bmw-x3-2023",
-        "/en/cars/1-bmw-x3-2023",
-        "/cars/5-1111-232323-7777",
-        "/ru/cars/5-1111-232323-7777",
-        "/en/cars/5-1111-232323-7777",
-      ],
-    },
+    rules: [
+      {
+        userAgent: ["AdsBot-Google", "AdsBot-Google-Mobile"],
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          // Deleted/non-existent car pages
+          "/cars/1-bmw-x3-2023",
+          "/ru/cars/1-bmw-x3-2023",
+          "/en/cars/1-bmw-x3-2023",
+          "/cars/5-1111-232323-7777",
+          "/ru/cars/5-1111-232323-7777",
+          "/en/cars/5-1111-232323-7777",
+        ],
+      },
+    ],
     sitemap: [`${base}/sitemap.xml`],
     host: base,
   };
